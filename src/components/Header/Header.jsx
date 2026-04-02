5dd4361f84d51ea32b91bae4ff09217cd0697aff
kdev_sobreira <kauam.morais09@gmail.com>
2026-04-02 00:47:55 -0300

    feat: create Header component and add to Home page

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    
+    return (
+        <header>
+            <h1>WikiPaises</h1>
+            <nav>
+                <button>Europe</button>
+                <button>Americas</button>
+                <button>Asia</button>
+                <button>Africa</button>
+                <button>Oceania</button>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react"
 import wikiService from "../../services/wiki.service"
 import Card from "../../components/Card/Card"
+import Header from "../../components/Header/Header"
 
 function Home (){
     const [country, setCountry] = useState([])
@@ -16,10 +17,14 @@ function Home (){
     
     return (
         <div>
-            {country.map(c => (
-            <Card key={c.cca3} country={c} />
-            ))}
+            <Header/>
+            <div className="country-list">
+                {country.map(c => (
+                    <Card key={c.cca3} country={c} />
+                ))}
+            </div>
         </div>
     )
 }
+
 export default Home
