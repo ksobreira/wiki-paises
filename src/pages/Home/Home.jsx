5cde87da30689fb93f73ba59a974406fdff83bcd
kdev_sobreira <kauam.morais09@gmail.com>
2026-04-01 22:55:23 -0300

    feat: fetch all countries from API on Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,22 @@
+import {useEffect, useState} from "react"
+import wikiService from "../../services/wiki.service"
+
 function Home (){
-    return <h1>Home</h1>
+    const [country, setCountry] = useState([])
+
+    useEffect(() => {
+        async function fecthData(){
+            const listCountry = await wikiService.getAllCountries();
+            console.log(listCountry)
+            setCountry(listCountry)
+        }
+        fecthData();
+    }, [])
+    
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
 }
 export default Home
--- src/services/wiki.service.js
+++ src/services/wiki.service.js
@@ -2,7 +2,7 @@ import wikiApi from "./wiki.api";
 
 const wikiService = {
     getAllCountries: async () => {
-        const response = await wikiApi.get("/all")
+        const response = await wikiApi.get("/all?fields=name,capital,region,population,flags,cca3")
         return response.data
     },
 
